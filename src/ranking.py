8dbe6a8841705d5947ff5d4eec00aa7b97677e17
Rodrigo <rodrigomendez99@gmail.com>
2021-11-18 17:33:04 +0100

    Segunda Implementación del Ranking

--- src/ranking.py
+++ src/ranking.py
@@ -7,8 +7,7 @@ class Ranking:
         try:
             self.gameLog = pk.load(open("rank.p", "rb"))
         except (OSError, IOError) as e:
-            self.gameLog = ""
-            pk.dump(self.gameLog, open("rank.p", "wb"))
+            self.gameLog = {}
 
     def getEasyRank(self):
         nuevoRank = {}
@@ -17,7 +16,7 @@ class Ranking:
                 nuevoRank[key] = [self.gameLog[key][0], self.gameLog[key][1]]
         return nuevoRank
 
-    def getMeidumRank(self):
+    def getMediumRank(self):
         nuevoRank = {}
         for key in self.gameLog.keys():
             if self.gameLog[key][1] == "Medio":
@@ -31,73 +30,94 @@ class Ranking:
                 nuevoRank[key] = [self.gameLog[key][0], self.gameLog[key][1]]
         return nuevoRank
 
-def test():
-
-    #puntuaciones = {"lion": [25, "Fácil"], "kitty": [17, "Medio"], "snake": [69, "Difícil"]}
-    #pk.dump(puntuaciones, open("rank.p", "wb"))
-
-    rank = Ranking()
-    indices = rank.gameLog.keys()
-    puntos = pd.Series(rank.gameLog.values())
-    puntos.index = indices
-
-    layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
-              [interf.Text(puntos.to_string())], [interf.Button("Fácil")], [interf.Button("Medio")],
-              [interf.Button("Difícil")], [interf.Button("Todas")], [interf.Button("Volver")]]
-    window = interf.Window("Ranking", layout, margins=(100, 50))
-
-    cond = True
-    while cond:
-        event, values = window.read()
-        # End program if user closes window or
-        # presses the OK button
-        if event == "Volver" or event == interf.WIN_CLOSED:
-            window.close()
-            cond = False
-        elif event == "Fácil":
-            nuevoRank = rank.getEasyRank()
-            indices = nuevoRank.keys()
-            puntos = pd.Series(nuevoRank.values())
+    def showRanking(self):
+        if self.gameLog != {}:
+            indices = self.gameLog.keys()
+            puntos = pd.Series(self.gameLog.values())
             puntos.index = indices
-
             layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
-                      [interf.Text(puntos.to_string())], [interf.Button("Fácil")], [interf.Button("Medio")],
-                      [interf.Button("Difícil")], [interf.Button("Todas")], [interf.Button("Volver")]]
-            window.close()
-            window = interf.Window("Ranking", layout, margins=(100, 50))
-        elif event == "Medio":
-            nuevoRank = rank.getMeidumRank()
-            indices = nuevoRank.keys()
-            puntos = pd.Series(nuevoRank.values())
-            puntos.index = indices
-
+                      [interf.Text(puntos.to_string())], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+        else:
             layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
-                      [interf.Text(puntos.to_string())], [interf.Button("Fácil")], [interf.Button("Medio")],
-                      [interf.Button("Difícil")], [interf.Button("Todas")], [interf.Button("Volver")]]
-            window.close()
-            window = interf.Window("Ranking", layout, margins=(100, 50))
-        elif event == "Difícil":
-            nuevoRank = rank.getHardRank()
-            indices = nuevoRank.keys()
-            puntos = pd.Series(nuevoRank.values())
-            puntos.index = indices
+                      [interf.Text("")], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+        window = interf.Window("Ranking", layout, margins=(100, 50))
 
-            layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
-                      [interf.Text(puntos.to_string())], [interf.Button("Fácil")], [interf.Button("Medio")],
-                      [interf.Button("Difícil")], [interf.Button("Todas")], [interf.Button("Volver")]]
-            window.close()
-            window = interf.Window("Ranking", layout, margins=(100, 50))
-        elif event == "Todas":
-            indices = rank.gameLog.keys()
-            puntos = pd.Series(rank.gameLog.values())
-            puntos.index = indices
+        cond = True
+        while cond:
+            event, values = window.read()
+            # End program if user closes window or
+            # presses the OK button
+            if event == "Volver" or event == interf.WIN_CLOSED:
+                window.close()
+                cond = False
+            elif event == "Fácil":
+                nuevoRank = self.getEasyRank()
+                if nuevoRank != {}:
+                    indices = nuevoRank.keys()
+                    puntos = pd.Series(nuevoRank.values())
+                    puntos.index = indices
 
-            layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
-                      [interf.Text(puntos.to_string())], [interf.Button("Fácil")], [interf.Button("Medio")],
-                      [interf.Button("Difícil")], [interf.Button("Todas")], [interf.Button("Volver")]]
-            window.close()
-            window = interf.Window("Ranking", layout, margins=(100, 50))
+                    layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
+                              [interf.Text(puntos.to_string())], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+                else:
+                    layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
+                              [interf.Text("")], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+                window.close()
+                window = interf.Window("Ranking", layout, margins=(100, 50))
+            elif event == "Medio":
+                nuevoRank = self.getMediumRank()
+                if nuevoRank != {}:
+                    indices = nuevoRank.keys()
+                    puntos = pd.Series(nuevoRank.values())
+                    puntos.index = indices
 
-# Press the green button in the gutter to run the script.
+                    layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
+                              [interf.Text(puntos.to_string())], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+                else:
+                    layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
+                              [interf.Text("")], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+                window.close()
+                window = interf.Window("Ranking", layout, margins=(100, 50))
+            elif event == "Difícil":
+                nuevoRank = self.getHardRank()
+                if nuevoRank != {}:
+                    indices = nuevoRank.keys()
+                    puntos = pd.Series(nuevoRank.values())
+                    puntos.index = indices
+
+                    layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
+                              [interf.Text(puntos.to_string())], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+                else:
+                    layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
+                              [interf.Text("")], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+                window.close()
+                window = interf.Window("Ranking", layout, margins=(100, 50))
+            elif event == "Todas":
+                if self.gameLog != {}:
+                    indices = self.gameLog.keys()
+                    puntos = pd.Series(self.gameLog.values())
+                    puntos.index = indices
+                    layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
+                              [interf.Text(puntos.to_string())], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+                else:
+                    layout = [[interf.Text("Ranking de Puntuaciones:")], [interf.Text("Nombre  [Puntos, Dificultad]")],
+                              [interf.Text("")], [interf.Button("Fácil"), interf.Button("Medio"),
+                      interf.Button("Difícil"), interf.Button("Todas"), interf.Button("Volver")]]
+                window.close()
+                window = interf.Window("Ranking", layout, margins=(100, 50))
+
+
 if __name__ == '__main__':
-    test()
+    puntuaciones = {"lion": [25, "Fácil"], "kitty": [17, "Medio"], "snake": [69, "Medio"]}
+    pk.dump(puntuaciones, open("rank.p", "wb"))
+    rank = Ranking()
+    rank.showRanking()
